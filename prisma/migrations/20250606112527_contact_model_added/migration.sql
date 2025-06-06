069878f9c4dc9671aad375a36c6d3c1ad2f7b0f5
Rohit Dhakane <rohitdhakane6@gmail.com>
2025-06-06 16:55:42 +0530

    Add Contact model and migration, removing User and Post tables

---
+++ prisma/migrations/20250606112527_contact_model_added/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "LinkPrecedence" AS ENUM ('primary', 'secondary');
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "contacts" (
+    "id" TEXT NOT NULL,
+    "phone_number" TEXT,
+    "email" TEXT,
+    "linked_id" INTEGER,
+    "link_precedence" "LinkPrecedence",
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
+);
